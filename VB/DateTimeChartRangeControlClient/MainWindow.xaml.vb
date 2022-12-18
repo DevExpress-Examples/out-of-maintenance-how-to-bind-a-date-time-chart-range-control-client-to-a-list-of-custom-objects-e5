51921e384cde469407744a88bd1856c52f30a14c
DevExpressExampleBot <fakeEmail>
2022-12-18 15:40:40 +0400

    Source auto update [skip ci]

--- VB/DateTimeChartRangeControlClient/MainWindow.xaml.vb
+++ VB/DateTimeChartRangeControlClient/MainWindow.xaml.vb
@@ -1,38 +1,39 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace DateTimeChartRangeControlClient
 
-	Public Class DateTimeItem
-		Public Property Argument() As Object
-		Public Property Value() As Object
-	End Class
+    Public Class DateTimeItem
 
-	Partial Public Class MainWindow
-		Inherits Window
+        Public Property Argument As Object
 
-		Private Const dataCount As Integer = 100
-		Private data As New List(Of DateTimeItem)()
+        Public Property Value As Object
+    End Class
 
-		Private Function GenerateDateTimeData() As List(Of DateTimeItem)
-			Dim now As DateTime = DateTime.Now.Date
-			Dim rand As New Random()
-			Dim value As Double = 0
-			For i As Integer = 0 To dataCount - 1
-				now = now.AddDays(1)
-				value += (rand.NextDouble() - 0.5)
-				data.Add(New DateTimeItem() With {
-					.Argument = now,
-					.Value = value + Math.Sin(i * 0.6)
-				})
-			Next i
-			Return data
-		End Function
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Me.DataContext = GenerateDateTimeData()
-		End Sub
-	End Class
+        Const dataCount As Integer = 100
+
+        Private data As List(Of DateTimeItem) = New List(Of DateTimeItem)()
+
+        Private Function GenerateDateTimeData() As List(Of DateTimeItem)
+            Dim now As Date = Date.Now.Date
+            Dim rand As Random = New Random()
+            Dim value As Double = 0
+            For i As Integer = 0 To dataCount - 1
+                now = now.AddDays(1)
+                value +=(rand.NextDouble() - 0.5)
+                data.Add(New DateTimeItem() With {.Argument = now, .Value = value + Math.Sin(i * 0.6)})
+            Next
+
+            Return data
+        End Function
+
+        Public Sub New()
+            Me.InitializeComponent()
+            DataContext = GenerateDateTimeData()
+        End Sub
+    End Class
 End Namespace
